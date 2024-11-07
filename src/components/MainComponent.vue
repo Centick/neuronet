7fd9a8e6d3af50b7967935a58102c6f485054942
Centick <utyatsky.ry@yandex.ru>
2024-11-08 00:43:21 +0300

    section 2 - 80%

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -48,16 +48,68 @@
 
     <div class="section">
         <div class="container opportunities__container">
+            <!-- 1 секция  -->
             <div class="opportunities_box opportunities_box--1">
                 <h3>Генерация <span class="violet-lite">текста</span>, анализ <span class="violet-lite">изображений</span> и <span class="violet-lite">файлов</span></h3>
-                <p>Последние версии нейросетей с анализом файлов могут обрпбатывать текста </p>
+                <p class="opportunities_box_text">Последние версии нейросетей с анализом файлов могут обрпбатывать текста с невероятной скоростью. <span class="violet-lite">Создавайте</span> логотипы, статьи, проекты <span class="violet-lite"> в 2 клика</span>.</p>
+                <div>
+                    <div class="wrap_for_files">
+                        <div class="box_for_files">
+                            <img src="../assets/img/file_types/a.svg" alt="file">
+                        </div>
+                    
+                        <div class="box_for_files">
+                            <img class="box_for_files__img-2" src="../assets/img/file_types/word.svg" alt="file">
+                        </div>
+
+                        <div class="box_for_files">
+                            <img class="box_for_files__img-2" src="../assets/img/file_types/excel.svg" alt="file">
+                        </div>
+
+                        <div class="box_for_files">
+                            <img src="../assets/img/file_types/pdf.svg" alt="file">
+                        </div>
+                        <div class="box_for_files">
+                            <img src="../assets/img/file_types/jpg.svg" alt="file">
+                        </div>
+                    </div>
+                    <p class="signature"><span class="violet">©</span> Не забывай о человеческих талантах...</p>
+                </div>
             </div>
-            <div class="opportunities_box opportunities_box--2"></div>
-            <div class="opportunities_box opportunities_box--3"></div>
-            <div class="opportunities_box opportunities_box--4"></div>
+
+            <!-- 2 секция  -->
+            <div class="opportunities_box opportunities_box--2">
+                
+            </div>
+
+            <!-- 3 секция -->
+            <div class="opportunities_box opportunities_box--3">
+                <h3>Создание реалистичных <span class="violet-lite">изображений</span> за считанные секунды</h3>
+                <p class="opportunities_box_text">Последние версии нейросетей с анализом файлов могут обрпбатывать текста с невероятной скоростью. <span class="violet-lite">Создавайте</span> логотипы, статьи, проекты <span class="violet-lite"> в 2 клика</span>.
+                </p>
+                <div class="opportunities_box--3_img"></div>
+            </div>
+
+            <!-- 4 секция -->
+            <div class="opportunities_box opportunities_box--4">
+                <h3 class="violet-lite">Нейросеть</h3>
+                <p class="opportunities_box--4_text">Построит таблицу, разметит код, нарисует уравнения</p>
+                <p>Более 10 видов разметки</p>
+                <div class="opportunities_box--4_bg-color"></div>
+            </div>
             <div class="opportunities_box opportunities_box--5"></div>
-            <div class="opportunities_box opportunities_box--6"></div>
-            <div class="opportunities_box opportunities_box--7"></div>
+
+            <!-- 6 секция -->
+            <div class="opportunities_box opportunities_box--6">
+                <h3><span class="violet-lite">Генерация аудио</span> из текста</h3>
+            </div>
+
+            <!-- 7 секция -->
+            <div class="opportunities_box opportunities_box--7">
+                <p>prod by free AI <span id="AiShedevrumBtn" class="violet-lite AiShedevrumBtn">"Шедеврум"</span></p>
+                <h3 class="opportunities_box--7_h3">Качественные <span class="violet-lite">видео</span> из текста и изображений</h3>
+                
+            </div>
         </div>
     </div>
 </template>
