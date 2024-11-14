a03a9581596ecc50b03432211459a1ae687bf702
Centick <utyatsky.ry@yandex.ru>
2024-11-14 22:46:43 +0300

    Revert "v7"
    This reverts commit 59a56f9094b6daaf64cfa7a0bca077d1159c56a2.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
 import './assets/style/main.css'
 
-// подключение основного css PriveVue
-import 'primevue/resources/themes/saga-blue/theme.css'; // Выберите тему
-import 'primevue/resources/primevue.min.css'; // Основные стили
-import 'primeicons/primeicons.css'; // Иконки
-
 import { createApp } from 'vue'
 
 // плавные анимации
@@ -12,22 +7,28 @@ import { createApp } from 'vue'
 // import 'aos/dist/aos.css'
 
 
-// import { createPinia } from 'pinia'  
+// import { createPinia } from 'pinia'
 
 import App from './App.vue'
-
-// Подключаем primeVue
-import PrimeVue from 'primevue/config';
+import { ImportsNotUsedAsValues } from 'typescript'
 // import router from './router'
 
 const app = createApp(App)
 
-app.use(PrimeVue);
-
-// инициализация анимациийq
+// инициализация анимациий
 // AOS.init();
 
 // app.use(createPinia())
 // app.use(router)
 
 app.mount('#app')
+
+
+
+// const wrapper = document.querySelector('#Banner_parallax-inner')
+//     const layers = document.querySelector("#paralaxTopGlow");
+
+//     wrapper.addEventListener('mousemove', initParallax);
+//     const initParallax = (e) => {
+//         paralaxTopGlow
+//     }
