0366a27706b11d9b7212ec3e5bb1767f5b5566dd
Centick <utyatsky.ry@yandex.ru>
2024-11-27 12:02:39 +0300

    aos

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,11 @@ import './assets/style/main.css'
 import { createApp } from 'vue'
 
 // плавные анимации
-// import AOS from 'aos'
-// import 'aos/dist/aos.css'
+import App from './App.vue'
+import 'aos/dist/aos.css'
 
 
 // import { createPinia } from 'pinia'
-
-import App from './App.vue'
 import { ImportsNotUsedAsValues } from 'typescript'
 // import router from './router'
 
@@ -23,12 +21,3 @@ const app = createApp(App)
 
 app.mount('#app')
 
-
-
-// const wrapper = document.querySelector('#Banner_parallax-inner')
-//     const layers = document.querySelector("#paralaxTopGlow");
-
-//     wrapper.addEventListener('mousemove', initParallax);
-//     const initParallax = (e) => {
-//         paralaxTopGlow
-//     }
